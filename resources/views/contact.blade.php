a07afcaef5bb6f9354fad3c81d6a0fdc4158eeca
Esraq Humayun <esraq26552@gmail.com>
2023-08-12 11:40:20 +0600

    Laravel custom login and registration

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>I AM FROM contact PAGE</h1>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+
+
+use App\Http\Controllers\AboutController;
+
+
+use App\Http\Controllers\ContactController;
+
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about_us', [AboutController::class, 'index']);
+
+Route::get('/contact', [ContactController::class, 'index']);
+
+
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+});
